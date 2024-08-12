93411facdd5bbaabfe8c14c3e36e48d723f07b8d
Smarika-tech <smarikapokhrel96@gmail.com>
2024-08-12 13:17:58 +0545

    designed

---
+++ script.js
@@ -0,0 +1,39 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let turnO = true;
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnO === true) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    console.log(pattern[0], pattern[1], pattern[2]);
+    console.log(
+      boxes[pattern[0]].innerText,
+      boxes[pattern[1]].innerText,
+      boxes[pattern[2]].innerText
+    );
+  }
+};
